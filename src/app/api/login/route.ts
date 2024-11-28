b9d7c4b13ead93b3ce6f83e8233bce22982cdab6
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2024-11-28 16:02:57 -0300

    adc

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,3 +1,16 @@
-export async function POST() {
-    
+export async function GET() {
+    const response = await fetch(`https://api.origamid.online/conta/login`, {
+        method: POST, 
+        headers: {
+                'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: 'dog',
+            password: 'dog'
+        })
+    });
+    if(!response.ok) {
+        return Response.json({erro: 'Dados incorretos'}, {status: 401})
+    }
+    return Response.json('true'); 
 }
