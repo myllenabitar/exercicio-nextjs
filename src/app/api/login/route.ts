7e7c8d136d52b1f65548d8cccc04a9cdd2ec1a67
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2024-11-29 12:52:18 -0300

    adc de alguns elementos

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,6 +1,8 @@
+import { cookies } from 'next/headers';
+
 export async function GET() {
     const response = await fetch(`https://api.origamid.online/conta/login`, {
-        method: POST, 
+        method: 'POST', 
         headers: {
                 'Content-Type': 'application/json'
         },
@@ -12,5 +14,11 @@ export async function GET() {
     if(!response.ok) {
         return Response.json({erro: 'Dados incorretos'}, {status: 401})
     }
-    return Response.json('true'); 
+
+    const data = await response.json();
+    (await cookies()).set('token', data.token,  {
+        httpOnly: true,
+        secure: true
+    })
+    return Response.json(data); 
 }
